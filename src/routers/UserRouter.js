28d9297d918a1f521aaf315b20099aabbe8703c1
revzankamarullah <revzankamarullah@gmail.com>
2017-01-31 10:03:13 +0700

    add post, put,delete method example

--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -7,5 +7,25 @@ router.get('/users',function (req,res) {
         res.json(data);
     })
 });
+router.get('/user/:id',function (req,res) {
+    UserService.getUser(req.params.id,function (data) {
+        res.json(data);
+    })
+});
+router.post('/user',function (req,res) {
+    UserService.addUser(req.body,function (data) {
+        res.json(data);
+    });
+});
+router.put('/user/:id',function (req,res) {
+    UserService.editUser(req.params.id,req.body,function (data) {
+        res.json(data);
+    })
+});
+router.delete('/user/:id',function (req,res) {
+    UserService.deleteUser(req.params.id,function (data) {
+        res.json(data);
+    })
+});
 
 module.exports = router;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -7,8 +7,54 @@ var UserService = {
                 callback({ status: false, message: err });
                 return;
             }
-            callback({status:true,message:"PEOPLE_TAKEN",data:data});
+            callback({status:true,message:"REQUEST_SUCCESS",data:data});
+        })
+    },
+    getUser:function (id,callback) {
+        UserModel.findById(id,function (err,data) {
+            if(err){
+                callback({ status: false, message: err });
+                return;
+            }
+            callback({status:true,message:"REQUEST_SUCCESS",data:data});
+        })
+    },
+    addUser: function (params,callback) {
+        var newUser = new UserModel({
+            name:params.name,
+            level:params.level
+        });
+
+        newUser.save(function (err,data) {
+            if(err){
+                callback({ status: false, message: err });
+                return;
+            }
+            callback({status:true,message:"REQUEST_SUCCESS",data:data});
+
+        })
+    },
+    editUser:function (id,params,callback) {
+        UserModel.findById(id,function (err,data) {
+            if(err){
+                callback({ status: false, message: err });
+                return;
+            }
+            data.name = params.name;
+            data.level = params.level;
+            data.save();
+            callback({status:true,message:"REQUEST_SUCCESS",data:data});
+        })
+    },
+    deleteUser:function (id,callback) {
+        UserModel.remove({_id:id},function (err,data) {
+            if(err){
+                callback({ status: false, message: err });
+                return;
+            }
+            callback({status:true,message:"REQUEST_SUCCESS"});
         })
     }
+
 };
 module.exports = UserService;
